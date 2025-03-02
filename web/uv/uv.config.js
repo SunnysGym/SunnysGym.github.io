16b5a0dfef5d5d725b69959075bf995f5b4a60f1
Sunny's Gym <sunnysgym1@gmail.com>
2025-03-01 22:13:26 -0500

    Update uv.config.js
    yay

--- web/uv/uv.config.js
+++ web/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/web/sunnys/gym/',
-    bare: 'https://sun.882310.xyz',
+    bare: 'https://sun.882310.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/web/uv/uv.handler.js',
